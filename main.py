85eabf1211481be396f90cf53b56cd57d97758a5
MaralSI <maral.sagynalieva@gmail.com>
2024-06-03 12:55:38 +0600

    new1.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-git init - инициализация
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+# git init - инициализация
 
 # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
