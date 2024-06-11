4272f9169a55e71587b3285556e442fe8007bf6e
MaralSI <maral.sagynalieva@gmail.com>
2024-06-11 19:27:39 +0600

    lessons5

---
+++ lessons/Lesson5.py
@@ -0,0 +1,27 @@
+
+# модули пайтона
+
+import random, time, math
+print (random.randint(1, 100))
+#
+#  7 85 10 6 4 50
+# 1-й модуль - встроенные
+
+from math import pi, e, sin, cos, tan
+print(pi)
+print(e)
+# print(sin)
+
+from lesson1 import Big0
+f=Big0()
+
+if __name__ == '__main__':
+    print(pi)
+    print(pi)
+    print(pi)
+
+import lesson
+
+
+
+# 2-й модуль -
